27f15a19b7357d19752fdfebd3c8fbf4fb26c46f
Gabriel Lauric <hallahelali@MacBook-Pro-de-HALLAH.local>
2025-08-25 09:42:44 +0200

    Référencement

--- monrestaurant/urls.py
+++ monrestaurant/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from restaurant.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('restaurant.urls')),  # Redirige toutes les URLs vers l’app restaurant
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
